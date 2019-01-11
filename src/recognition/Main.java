a3c7974ebff2949bf1a439f295b97ab97e7656de
Sergei Khmelevskiy <xmelko91@gmail.com>
2019-01-11 21:39:19 +0300

    First neuron :)

--- src/recognition/Main.java
+++ src/recognition/Main.java
@@ -1,7 +1,22 @@
 package recognition;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        int[] weight = {2, 1, 2, 4, -4, 4, 2, -1, 2};
+        String str = "";
+        Scanner sc = new Scanner(System.in);
+        str += sc.nextLine() + sc.nextLine() + sc.nextLine();
+        int sum = -5;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '_')
+                sum+= weight[i];
+        }
+        if (sum > 0)
+            System.out.print("This is 0!");
+        else
+            System.out.println("This is 1!");
+        //System.out.println(sum);
     }
 }
