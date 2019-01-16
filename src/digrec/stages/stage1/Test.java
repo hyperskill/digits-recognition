6d8e9a045a8f3508b74ce55c14a9163f9aafb09d
AzartA <ershovna@mail.ru>
2019-01-16 23:46:15 +0300

    Solved first stage.
    Signed-off-by: AzartA <ershovna@mail.ru>

--- src/digrec/stages/stage1/Test.java
+++ src/digrec/stages/stage1/Test.java
@@ -1,9 +1,22 @@
 package digrec.stages.stage1;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int [] neuWeight = {2,1,2,4,-4,4,2,-1,2,-5};
+		int a10 = 0; 	
+		System.out.println("enter matrix 9x9, where 1 is blue and 0 is white:");
+		for (int i=0;i<9;i++) {
+			a10	+= sc.nextInt()*neuWeight[i];
+		}
+		a10+=neuWeight[9];
+		 
+		System.out.println(a10<0?"It's a \"1\".":"It's a \"0\".");
+		
 
 	}
 
