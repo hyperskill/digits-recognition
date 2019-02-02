33f2593b731369390ca7f2b368f2261fe2af2efa
serge <gyrsergey@gmail.com>
2019-02-02 19:50:37 +0400

    Recognize handwritten 0 and 1

--- src/recognition/Main.java
+++ src/recognition/Main.java
@@ -1,7 +1,38 @@
 package recognition;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.println("Input 3x3 grid ('_' for white, any other for blue):");
+        int[] w = { 2,1,2,4, -4, 4 , 2, -1, 2,-5};
+        int sum = 0;
+        byte[] mask = new byte[9];
+
+
+        Scanner scanner = new Scanner(System.in);
+        byte counter = 0;
+        while((counter!=9)&&(scanner.hasNext()))
+        {
+           String str = scanner.next();
+           for(int i = 0; i< str.length(); i++)
+           {
+               if(str.charAt(i)!=' ')
+               {
+                   if(str.charAt(i)=='_')
+                       mask[counter++]=0;
+                   else
+                       mask[counter++]=1;
+
+               }
+
+           }
+        }
+        for(int i = 0; i < 9; i++)
+        {
+            sum += mask[i]*w[i];
+        }
+        sum+=w[9];
+        System.out.println(sum>=0?0:1);
     }
 }
