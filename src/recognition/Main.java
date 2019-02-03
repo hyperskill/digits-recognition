093cf6df72aae3b55c644d865c4fdc4bff5ee1be
serge <gyrsergey@gmail.com>
2019-02-03 15:53:04 +0400

    Recognize handwritten 0 to 9

--- src/recognition/Main.java
+++ src/recognition/Main.java
@@ -2,17 +2,33 @@ package recognition;
 
 import java.util.Scanner;
 
+
+
 public class Main {
+
+    public static int[][] w = {
+            {1,1,1,1,-1,1,1,-1,1,1,-1,1,1,1,1,-1},
+            {-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,0,6},
+            {1,1,1,-1,-1,1,1,1,1,1,-1,-1,1,1,1,0},
+            {1,1,1,-1,-1,1,1,1,1,-1,-1,1,1,1,1,0},
+            {1,-1,1,1,-1,1,1,1,1,-1,-1,1,-1,-1,1,2},
+            {1,1,1,1,-1,-1,1,1,1,-1,-1,1,1,1,1,0},
+            {1,1,1,1,-1,-1,1,1,1,1,-1,1,1,1,1,-1},
+            {1,1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,4},
+            {1,1,1,1,-1,1,1,1,1,1,-1,1,1,1,1,-2},
+            {1,1,1,1,-1,1,1,1,1,-1,-1,1,1,1,1,-1}
+    };
+
     public static void main(String[] args) {
-        System.out.println("Input 3x3 grid ('_' for white, any other for blue):");
-        int[] w = { 2,1,2,4, -4, 4 , 2, -1, 2,-5};
-        int sum = 0;
-        byte[] mask = new byte[9];
+        System.out.println("Input 3x5 grid ('_' for white, any other for blue):");
+
+        int[] sum = new int[10];
+        byte[] mask = new byte[15];
 
 
         Scanner scanner = new Scanner(System.in);
         byte counter = 0;
-        while((counter!=9)&&(scanner.hasNext()))
+        while((counter!=15)&&(scanner.hasNext()))
         {
            String str = scanner.next();
            for(int i = 0; i< str.length(); i++)
@@ -28,11 +44,25 @@ public class Main {
 
            }
         }
-        for(int i = 0; i < 9; i++)
-        {
-            sum += mask[i]*w[i];
+
+        for(int j =0 ; j < 10; j++) {
+            for (int i = 0; i < 16; i++) {
+                if(i<15)
+                    sum[j] += mask[i] * w[j][i];
+                else
+                    sum[j] +=w[j][i];
+            }
         }
-        sum+=w[9];
-        System.out.println(sum>=0?0:1);
+
+        int max = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            if(sum[i]>sum[max])
+            {
+                max = i;
+            }
+        }
+
+        System.out.println(max);
     }
 }
